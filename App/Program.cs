992e81bba405ff1813aac5d0ac6034b0d281f7d9
MHanzalaJamil <muhammadhanzalaj@gmail.com>
2025-01-13 10:18:48 +0500

    Update #2

--- App/Program.cs
+++ App/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello\n");
+Console.WriteLine(1);
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello\n");
